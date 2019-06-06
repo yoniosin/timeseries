ac8ef88ab68bca07d8366442627020b746d86215
yoniosin <yoni.osin@gmail.com>
2019-06-06 16:19:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -16,6 +16,6 @@ class SubjectMetaData:
 	bold_mat_path: str
 	
 	def __post_init__(self):
-		self.watch_times = list(map(window_times, self.watch_on, self.watch_duration))
-		self.regulate_times = list(map(window_times, self.regulate_on, self.regulate_duration))
+		self.watch_times = map(window_times, self.watch_on, self.watch_duration)
+		self.regulate_times = map(window_times, self.regulate_on, self.regulate_duration)
 	
